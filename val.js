891804c1ff05a0378f30b407cd49688024b9b5aa
Shriram26101999 <108632713+Shriram26101999@users.noreply.github.com>
2023-02-26 00:13:26 +0530

    Add files via upload

---
+++ val.js
@@ -0,0 +1,43 @@
+
+let wel = document.getElementById("welcom1");
+let mainInfo = JSON.parse(localStorage.getItem("users"));
+let dataCurr = JSON.parse(localStorage.getItem("currentUser"));
+let nCurr = dataCurr[0].username;
+let email = dataCurr[0].email;
+displayinfo(nCurr, email);
+function teacherUpdate() {
+  var pwd = document.getElementById("pwd").value;
+  let pwdN = document.getElementById("pwdN").value;
+  let pwdC = document.getElementById("pwdC").value;
+  let validation =
+  mainInfo.length &&
+    JSON.parse(localStorage.getItem("users")).some(
+      (data) => data.email.toLowerCase() == email && data.pass == pwd
+    );
+  if (!validation) {
+    alert("Error: Incorrect password !!");
+  } else {
+    if (pwdN == pwdC) {
+      usrIndex = mainInfo.findIndex((obj) => obj.pass == pwd);     
+      mainInfo[usrIndex].pass = pwdC;
+      console.log(mainInfo);
+      localStorage.setItem("users", JSON.stringify(mainInfo));
+      crntUsrIndex = dataCurr.findIndex((obj) => obj.pass == pwd);     
+      dataCurr[usrIndex].pass = pwdC;
+      alert("Too Stronge Password updated.")
+      console.log(dataCurr);
+      localStorage.setItem("users", JSON.stringify(dataCurr));
+    }
+  }
+}
+
+
+function teacherExit() {
+  window.location.href = "index.html";
+  localStorage.removeItem("currentUser");
+}
+
+function displayinfo(name, email) {
+  console.log(name, email);
+  wel.innerHTML = `<p>Welcome Back: ${name}</p> <p>Your Email-ID: ${email}</p>`;
+}
